757a04589a6162bf838291fde395bb437f379b68
sanjutalukder <talukdersanju55@gmail.com>
2025-07-15 13:58:22 +0300

    Update sura.js

--- Script/commands/sura.js
+++ Script/commands/sura.js
@@ -22,19 +22,19 @@ const fs = global.nodemodule["fs-extra"];
    var hi = ["•┄┅════❁🌺❁════┅┄•\n\nপ্রিয় মুসলিম |ভাই ও বন| সূরা টি শুনো তুমার প্রান জুরিয়ে যাবে \n\n ইনশাআল্লাহ ❤️🌸 \n\n 𝐌𝐮𝐬𝐥𝐢𝐦 𝐇𝐚𝐤𝐜𝐞𝐫 𝐁𝐃\n\n•┄┅════❁🌺❁════┅┄•"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
-   "https://drive.google.com/uc?id=1Ml6znasS_cajYJVS8OJ19DQO6aaLzWkc",
+   "https://drive.google.com/uc?export=download&id=16zQ_3jbg1XJr9Ck7LRLYo8dEd2WpqPBx",
 
-"https://drive.google.com/uc?id=1NKyRitWSGriX3TG23YTLj0tgfySwn6Q-",
+"https://drive.google.com/uc?export=download&id=16zQ_3jbg1XJr9Ck7LRLYo8dEd2WpqPBx",
 
-"https://drive.google.com/uc?id=1N-sbqx4LjEc-OOEa0MXhM2crzyvn3ynj",
+"https://drive.google.com/uc?export=download&id=16zQ_3jbg1XJr9Ck7LRLYo8dEd2WpqPBx",
 
-"https://drive.google.com/uc?id=1N9AzB4zAWlz2bG3UesZ7GawyJykRO79s",
+"https://drive.google.com/uc?export=download&id=16zQ_3jbg1XJr9Ck7LRLYo8dEd2WpqPBx",
 
-"https://drive.google.com/uc?id=1MrLaZG9NyfSDLjZvCRK69L0nnepV6R7U",
+"https://drive.google.com/uc?export=download&id=16zQ_3jbg1XJr9Ck7LRLYo8dEd2WpqPBx",
 
-"https://drive.google.com/uc?id=1N7W-i_Xr3lxM0cvv4dQlGUvsFGoyHnIl",
+"https://drive.google.com/uc?export=download&id=16zQ_3jbg1XJr9Ck7LRLYo8dEd2WpqPBx",
 
-"https://drive.google.com/uc?id=1Mn8JXddjoYKHkNcgAdnw8dnwhr2Ems6s",
+"https://drive.google.com/uc?export=download&id=16zQ_3jbg1XJr9Ck7LRLYo8dEd2WpqPBx",
 
 "https://drive.google.com/uc?id=1NLbrtpig80X1_NTlRHmeKG7ZQPtTmdTJ",
 
